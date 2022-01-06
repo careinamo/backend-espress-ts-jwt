7b6b350406ba480387be49a0c2044205b6fd2442
christian areinamo <careinamoareinamo@gmail.com>
2022-01-06 13:53:25 -0500

    add docker for prod and dev

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log("hello world!!");
+import express from 'express';
+
+const app = express();
+
+// respond with "hello world" when a GET request is made to the homepage
+app.get('/hello', function (req, res) {
+  res.send('hello world!!')
+})
+
+app.listen(3000, () => {
+  console.log('server running on port 3000');
+});
