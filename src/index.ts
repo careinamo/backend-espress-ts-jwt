595c3b08361a1e3e27d715a04cdf9445b77ee00f
christian areinamo <careinamoareinamo@gmail.com>
2022-01-08 00:23:11 -0500

    add dotenv

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
+import dotenv from 'dotenv-safe';
 import express from 'express';
+
+dotenv.config();
 
 const app = express();
 
-// respond with "hello world" when a GET request is made to the homepage
 app.get('/hello', (req, res) => {
-  res.send('hello world!!');
+  res.send(`hello world!! from ${process.env.APP_NAME}`);
 });
 
 app.listen(3000);
