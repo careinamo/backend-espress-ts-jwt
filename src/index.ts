5fc12cadd24f92609af6348d5e594f7bd3877bd3
christian areinamo <careinamoareinamo@gmail.com>
2022-01-07 23:42:24 -0500

    add eslint and prettier config

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,8 @@ import express from 'express';
 const app = express();
 
 // respond with "hello world" when a GET request is made to the homepage
-app.get('/hello', function (req, res) {
-  res.send('hello world!!')
-})
-
-app.listen(3000, () => {
-  console.log('server running on port 3000');
+app.get('/hello', (req, res) => {
+  res.send('hello world!!');
 });
+
+app.listen(3000);
